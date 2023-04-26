0858d8aeb2636e947e155f898aff63c6ade099ed
Reginald Ford <631752+reginaldford@users.noreply.github.com>
2023-04-26 03:44:43 -0400

    fixing stuff

--- get_micro.sh
+++ get_micro.sh
@@ -1,3 +1,4 @@
 curl https://getmic.ro > script_from_the_web.sh
 echo "Check script_from_the_web.sh and then run it."
-
+echo 1e6a415e6413cfbb88dbcf0b28cc0836 was the previous md5sum
+md5sum script_from_the_web.sh 
--- install_rexist.sh
+++ install_rexist.sh
@@ -1,4 +1,4 @@
 # Copies the rexist.micro and settings.json file to .config/micro
-mkdir -pv ~/.config/micro
-cp -fv rexist.micro ~/.config/micro/
+mkdir -pv ~/.config/micro/colorschemes
+cp -fv rexist.micro ~/.config/micro/colorschemes/
 cp -fv settings.json ~/.config/micro/
